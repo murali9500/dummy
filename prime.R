1f40f8e166604b2c559e13f393559dc566a6f1d8
murali9500 <167340661+murali9500@users.noreply.github.com>
2024-04-18 07:06:46 +0530

    Add files via upload

---
+++ prime.R
@@ -0,0 +1,19 @@
+num = as.integer(readline()) 
+flag = 0 
+if(num> 1) { 
+  flag = 1 
+  for(i in 2:(num-1)) { 
+    if ((num %% i) == 0) { 
+      flag = 0 
+      break 
+      23 
+      24  
+    } 
+  } 
+}  
+if(num == 2)    flag = 1 
+if(flag == 1) { 
+  print(paste(num,"is a prime number")) 
+} else { 
+  print(paste(num,"is not a prime number")) 
+} 
